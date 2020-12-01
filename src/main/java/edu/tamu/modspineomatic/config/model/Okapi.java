ee12a679023a4f26d050d398d4400cb89d22fe0a
Ryan Laddusaw <rladdusaw@gmail.com>
2020-12-01 08:59:48 -0600

    Using lombok annotations

--- src/main/java/edu/tamu/modspineomatic/config/model/Okapi.java
+++ src/main/java/edu/tamu/modspineomatic/config/model/Okapi.java
@@ -3,6 +3,9 @@ package edu.tamu.modspineomatic.config.model;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
+@Data
 @Component
 @ConfigurationProperties(prefix = "okapi")
 public class Okapi {
@@ -10,25 +13,5 @@ public class Okapi {
     private String url;
 
     private Credentials credentials;
-
-    public Okapi() {
-
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Credentials getCredentials() {
-        return credentials;
-    }
-
-    public void setCredentials(Credentials credentials) {
-        this.credentials = credentials;
-    }
 
 }
