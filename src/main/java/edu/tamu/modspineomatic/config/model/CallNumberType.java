936d25af75ee72ab81465ba058cc12e6016322d3
Ryan Laddusaw <rladdusaw@gmail.com>
2021-01-04 07:38:13 -0600

    Converted call number type to new reference strategy

---
+++ src/main/java/edu/tamu/modspineomatic/config/model/CallNumberType.java
@@ -0,0 +1,12 @@
+package edu.tamu.modspineomatic.config.model;
+
+import lombok.Data;
+
+@Data
+public class CallNumberType {
+
+    private String id;
+    private String type;
+    private String desc;
+
+}
--- src/main/java/edu/tamu/modspineomatic/service/TemplatingService.java
+++ src/main/java/edu/tamu/modspineomatic/service/TemplatingService.java
@@ -3,8 +3,6 @@ package edu.tamu.modspineomatic.service;
 import java.util.Locale;
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
 import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
+import edu.tamu.modspineomatic.config.model.CallNumberType;
+import edu.tamu.modspineomatic.config.model.CallNumberTypeReference;
 import edu.tamu.modspineomatic.config.model.Location;
 import edu.tamu.modspineomatic.config.model.LocationReference;
 
@@ -21,6 +21,7 @@ public class TemplatingService {
     private static final String SPINE_LABEL = "spine-label";
     private static final String CHRONOLOGY = "chronology";
     private static final String CALL_NUMBER = "call_number";
+    private static final String CALL_NUMBER_PREFIX = "call_number_prefix";
     private static final String PARSED_CALL_NUMBER = "parsed_call_number"; 
     private static final String CALL_NUMBER_TYPE = "call_number_type";
     private static final String CALL_NUMBER_TYPE_DESC = "call_number_type_desc";
@@ -29,15 +30,18 @@ public class TemplatingService {
     private static final String CHRONOLOGY_PROPERTY = "chronology";
     private static final String EFFECTIVE_CALL_NUMBER_PROPERTY = "effectiveCallNumberComponents";
     private static final String CALL_NUMBER_PROPERTY = "callNumber";
+    private static final String CALL_NUMBER_PREFIX_PROPERTY = "prefix";
     private static final String CALL_NUMBER_TYPE_PROPERTY = "typeId";
     private static final String EFFECTIVE_LOCATION_PROPERTY = "effectiveLocationId";
-    private static final String PARSED_CALL_NUMBER_PROPERTY = "parsedCallNumber";
 
     @Autowired
     private SpringTemplateEngine templateEngine;
 
     @Autowired
-    private LocationReference reference;
+    private CallNumberTypeReference callNumberTypeReference;
+
+    @Autowired
+    private LocationReference locationReference;
 
     public String templateResponse(JsonNode itemNode) {
         return templateEngine.process(SPINE_LABEL, populateContext(itemNode));
@@ -49,42 +53,25 @@ public class TemplatingService {
         String chron = itemNode.get(CHRONOLOGY_PROPERTY) != null ? itemNode.get(CHRONOLOGY_PROPERTY).asText() : "";
         context.setVariable(CHRONOLOGY, chron);
         context.setVariable(CALL_NUMBER, itemNode.get(EFFECTIVE_CALL_NUMBER_PROPERTY).get(CALL_NUMBER_PROPERTY).asText());
-        // TODO: parse call number
+        context.setVariable(CALL_NUMBER_PREFIX, itemNode.get(EFFECTIVE_CALL_NUMBER_PROPERTY).get(CALL_NUMBER_PREFIX).asText());
+        // TODO: parse call number if needed
         context.setVariable(PARSED_CALL_NUMBER, itemNode.get(EFFECTIVE_CALL_NUMBER_PROPERTY).get(CALL_NUMBER_PROPERTY).asText());
 
         String callNumberUUID = itemNode.get(EFFECTIVE_CALL_NUMBER_PROPERTY).get(CALL_NUMBER_TYPE_PROPERTY).asText();
-        // Fall back to other type
-        String callNumberType = "4";
-        String callNumberTypeDesc = "Other";
 
+        Optional<CallNumberType> callNumberTypeOption = callNumberTypeReference.getCallNumberTypes().stream()
+            .filter(c -> c.getId().equals(callNumberUUID))
+            .findAny();
 
-        switch (callNumberUUID) {
-            case "03dd64d0-5626-4ecd-8ece-4531e0069f35":
-                callNumberType = "1";
-                callNumberTypeDesc = "Dewey";
-                break;
-            case "054d460d-d6b9-4469-9e37-7a78a2266655":
-                callNumberType = "0";
-                callNumberTypeDesc = "Library of Congress";
-                break;
-            case "95467209-6d7b-468b-94df-0f5d7ad2747d":
-                callNumberType = "0";
-                callNumberTypeDesc = "National Library of Medicine";
-                break;
-            case "fc388041-6cd0-4806-8a74-ebe3b9ab4c6e":
-                callNumberType = "3";
-                callNumberTypeDesc = "Superintendent of Documents";
-                break;
-            default:
-                break;
+        if (callNumberTypeOption.isPresent()) {
+            CallNumberType cnType = callNumberTypeOption.get();
+            context.setVariable(CALL_NUMBER_TYPE, cnType.getType());
+            context.setVariable(CALL_NUMBER_TYPE_DESC, cnType.getDesc());
         }
-
-        context.setVariable(CALL_NUMBER_TYPE, callNumberType);
-        context.setVariable(CALL_NUMBER_TYPE_DESC, callNumberTypeDesc);
 
         String locationUUID = itemNode.get(EFFECTIVE_LOCATION_PROPERTY).asText();
 
-        Optional<Location> locationOption = reference.getLocations().stream()
+        Optional<Location> locationOption = locationReference.getLocations().stream()
             .filter(l -> l.getId().equals(locationUUID))
             .findAny();
 
